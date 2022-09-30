4c807f1c374f7216f9062ab81b4bdb76bda2947e
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-09-30 21:01:41 +0100

    Global variables assigned

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random, time
+
+grid = [[]]
+map_size = 15
+num_ships = 4
+bullets_left = 25
+game_over = False
+ships_sunk = 0
+ship_placement = [[]]
+grid_letters = "ABCDEFGHIJ"
+
