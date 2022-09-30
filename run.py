3df762930b4e805525afc1e9fcaf6a7263d15e07
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-09-30 21:11:23 +0100

    Starting the grid formation function

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import random, time
+import random
 
 grid = [[]]
 map_size = 15
@@ -9,3 +9,14 @@ ships_sunk = 0
 ship_placement = [[]]
 grid_letters = "ABCDEFGHIJ"
 
+def generate_map():
+    
+    rows, cols = (map_size, map_size)
+    grid = []
+    for r in range(rows):
+        row = []
+        for c in range(cols):
+            row.append("~")
+        grid.append(row)
+           
+ships_placed = 0
